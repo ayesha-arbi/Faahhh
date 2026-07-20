76a6123c36406ea22dfe85ae97fe92861517111c
ayesha-arbi <ayeshaarbi7@gmail.com>
2026-07-21 00:22:03 +0500

    4

---
+++ script_3.js
@@ -0,0 +1,12 @@
+let score ="30aaa"
+
+console.log(typeof score)
+
+let a =Number(score)
+console.log(typeof a)
+console.log(a)
+
+let log;
+let booleanlog = Boolean(log)
+console.log(typeof booleanlog)
+console.log(booleanlog)
