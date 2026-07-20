3b2791ece97952e8b2e012c416002d875dfcae3e
ayesha-arbi <ayeshaarbi7@gmail.com>
2026-07-21 00:09:43 +0500

    4,5

---
+++ script._1.js
@@ -0,0 +1,11 @@
+const accountId = 121434
+let accountEmail="ayesha@gmail.com"
+var accountPassword = "123"
+accountCity="karachi"
+
+
+accountEmail="ayeshaaaa@gmail.com"
+accountPassword = "123443"
+accountCity="karachihehehee"
+
+console.table([accountId, accountEmail])
---
+++ script_2.js
@@ -0,0 +1,9 @@
+"use strict";//treat all js code as newer version
+
+// we r using node js not browser alert("hello") 
+
+let name = "hitesh"
+let age=18
+let isloggedin=undefined
+
+console.log(typeof(isloggedin))
