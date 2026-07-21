69d2740eb1cd83ee39da03322c7db2a2bd067762
ayesha-arbi <ayeshaarbi7@gmail.com>
2026-07-21 18:34:56 +0500

    vid 9/51

---
+++ datatypes.js
@@ -0,0 +1,10 @@
+// premitive
+
+
+//7 types of primitive (call by value) - changes in copy
+// String,number,boolean, null,undefined,symbol,BigInt
+//symbol-to make a data unique 
+
+// Non-primitive 
+//types of non-primitive (call by reference)-changes in original value
+// Array ,objects, Functions
