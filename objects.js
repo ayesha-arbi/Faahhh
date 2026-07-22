543c1d9d70d137847cb00e7b878ed3dbafff9d8c
ayesha-arbi <ayeshaarbi7@gmail.com>
2026-07-22 12:26:44 +0500

    rcrt

---
+++ objects.js
@@ -0,0 +1,14 @@
+//singleton
+
+//object.create
+
+//object literals
+const jsuser ={
+    name: "hitesh",
+    fullname: "ayesha zahid aarbie",
+    age: "Jaipur",
+    location: "karachi",
+    email:"ayeshazahid",
+    islogin: true,
+    lastlogindays: ["Monday","Saturday"]
+}
